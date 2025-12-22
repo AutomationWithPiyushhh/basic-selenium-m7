7d57753a1f98f25f3daf7feab82930677bd4673f
AutomationWithPiyushhh <vasudevahir21@gmail.com>
2025-12-22 11:39:06 +0530

    TKS

---
+++ basic-selenium-m7/src/test/java/working_with_SS/Facebook.java
@@ -0,0 +1,46 @@
+package working_with_SS;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+public class Facebook {
+	public static void main(String[] args) throws IOException {
+		WebDriver driver = new ChromeDriver();
+		
+		
+		driver.get("https://www.facebook.com/");
+		
+//		1> downcasting
+		TakesScreenshot tks = (TakesScreenshot) driver;
+		
+//		2> take the ss in file format
+		File source = tks.getScreenshotAs(OutputType.FILE);
+		
+//		3> create the java representation object of the physical file
+		File destination = new File("C:/Users/User/git/Basic-Sel-M7/basic-selenium-m7/src/test/java/working_with_SS/dummy.png");
+		
+//		4> copy that content to the dummy file
+		FileHandler.copy(source, destination);
+		
+		
+		driver.quit();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
