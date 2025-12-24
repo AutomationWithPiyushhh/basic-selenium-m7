900cb9e7a74d7c8d7a9bdbbd3124cf5dd86399eb
AutomationWithPiyushhh <vasudevahir21@gmail.com>
2025-12-24 13:07:54 +0530

    jse

--- basic-selenium-m7/src/test/java/working_with_JSE/Zomato.java
+++ basic-selenium-m7/src/test/java/working_with_JSE/Zomato.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Zomato {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
@@ -16,6 +16,35 @@ public class Zomato {
 		
 //		1> typecaste to JSE 
 		JavascriptExecutor jse = (JavascriptExecutor) driver;
-		jse.executeScript("window.location='https://www.zomato.com/'");
+		
+		jse.executeScript("window.location='https://www.zomato.com/';");
+		
+////		scrollBy
+//		jse.executeScript("window.scrollBy(0,500);");
+//		Thread.sleep(1000);
+//		jse.executeScript("window.scrollBy(0,500);");
+//		Thread.sleep(1000);
+//		jse.executeScript("window.scrollBy(0,500);");
+//		Thread.sleep(1000);
+//
+////		scrollTo
+//		jse.executeScript("window.scrollTo(0,500);");
+//		Thread.sleep(1000);
+//		jse.executeScript("window.scrollTo(0,500);");
+//		Thread.sleep(1000);
+//		jse.executeScript("window.scrollTo(0,500);");
+//		Thread.sleep(1000);
+		
+//		String title = driver.getTitle();
+		
+		String title = (String) jse.executeScript("return document.title;");
+		
+		System.out.println(title);
+		
+		
+		
+		
+		
+		driver.quit();
 	}
 }
